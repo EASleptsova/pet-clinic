906fe1f5c9f88f4d23d8b381bafcf9a422243226
esleptsova <Unik.SE>
2020-12-02 09:24:25 +0000

    Initial Project Add

---
+++ src/test/java/com/lacygift/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacygift.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
