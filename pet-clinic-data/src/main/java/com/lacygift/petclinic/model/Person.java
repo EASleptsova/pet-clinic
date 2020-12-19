e0da0e65af270de0af344fedbca34e286344afb1
esleptsova <Unik.SE>
2020-12-19 11:09:05 +0000

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/com/lacygift/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/lacygift/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.lacygift.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private  String lastName;
--- pet-clinic-data/src/main/java/com/lacygift/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/lacygift/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.lacygift.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
