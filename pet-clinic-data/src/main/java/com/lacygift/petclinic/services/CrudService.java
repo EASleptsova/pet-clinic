65b0d0cddaa5bc2ac7e6fae79b61ca166259938a
esleptsova <Unik.SE>
2020-12-21 11:47:16 +0000

    implement map based services, closes #6

--- pet-clinic-data/src/main/java/com/lacygift/petclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/com/lacygift/petclinic/services/CrudService.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public interface CrudService <T, ID>{
     Set<T> findAll();
     T findById(ID id);
-    T save (T entity);
-    void delete(T entity);
+    T save (T object);
+    void delete(T object);
     void deleteById(ID id);
 }
---
+++ pet-clinic-data/src/main/java/com/lacygift/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.lacygift.petclinic.services.map;
+
+import com.lacygift.petclinic.model.Vet;
+import com.lacygift.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        super.save(object.getId(), object);
+        return object;
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
