ddc1e3e6e3ce2d9795460e8971f5e7274738f147
esleptsova <Unik.SE>
2021-01-26 10:29:28 +0000

    added pets and contact info on startup, closes #33

--- pet-clinic-web/src/main/java/com/lacygift/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/lacygift/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.lacygift.petclinic.bootstrap;
 
 import com.lacygift.petclinic.model.Owner;
+import com.lacygift.petclinic.model.Pet;
 import com.lacygift.petclinic.model.PetType;
 import com.lacygift.petclinic.model.Vet;
 import com.lacygift.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.lacygift.petclinic.services.PetTypeService;
 import com.lacygift.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -35,12 +38,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1= new Owner();
         owner1.setFirstName("Maria");
         owner1.setLastName("Demidova");
+        owner1.setAddress("123 Street");
+        owner1.setCity("London");
+        owner1.setTelephone("564526369");
+
+        Pet pet1= new Pet();
+        pet1.setPetType(savedDogPetType);
+        pet1.setOwner(owner1);
+        pet1.setBirthdate(LocalDate.now());
+        pet1.setName("Rosco");
+        owner1.getPets().add(pet1);
 
         ownerService.save(owner1);
 
         Owner owner2= new Owner();
         owner2.setFirstName("Anna");
         owner2.setLastName("Ivanova");
+        owner2.setAddress("456 Street");
+        owner2.setCity("London");
+        owner2.setTelephone("5687595262");
+
+        Pet pet2= new Pet();
+        pet2.setPetType(savedCatPetType);
+        pet2.setOwner(owner2);
+        pet2.setBirthdate(LocalDate.now());
+        pet2.setName("Garfield");
+        owner2.getPets().add(pet2);
 
         ownerService.save(owner2);
 
